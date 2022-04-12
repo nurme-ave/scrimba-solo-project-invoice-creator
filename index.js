f8d7268f0a5f7fa81d51fdcc3aea98defd6f036e
Ave Nurme <nurme.ave@gmail.com>
2022-04-13 02:55:16 +0300

    Start with adding the task removal functionality

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
  */
 
 const taskButtons = document.getElementById('task-buttons');
+const removeButton = document.getElementById('remove-button')
 const invoicedTasks = new Set();
 
 let services = [
@@ -25,12 +26,21 @@ taskButtons.innerHTML = taskButtonsHtml.join('');
 
 
 taskButtons.addEventListener('click', (e) => {
-  let target = e.target;
+  const target = e.target;
   if (target.tagName === 'BUTTON') {
     invoicedTasks.add(services[target.value]);
     renderTaskList(invoicedTasks);
   }
 });
+
+removeButton.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.tagName === 'BUTTON') {
+    removeItem(target.id)
+    console.log(target.id)
+    renderTaskList(invoicedTasks)
+  }
+})
 
 
 function renderTaskList(taskList) {
@@ -38,11 +48,15 @@ function renderTaskList(taskList) {
   let priceHtml = '';
   let totalAmount = 0;
   for (let item of taskList) {
-    tasksHtml += `<p class="task">${item.task}<button class="task-remove">Remove</button></p>`;
+    tasksHtml += `<p class="task">${item.task}<button id="${item.id}" class="task-remove">Remove</button></p>`;
     priceHtml += `<p class="price"><span class="currency-sign">$</span> ${item.price}</p>`;
     totalAmount += item.price;
   }
   document.getElementById('tasks').innerHTML = tasksHtml;
   document.getElementById('prices').innerHTML = priceHtml;
   document.getElementById('total-amount').innerHTML = `<p class="price-total"><span class="currency-sign-total-amount">$ ${totalAmount}</span></p>`;
+}
+
+
+function removeItem(item){
 }
