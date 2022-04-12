79b5cb988faa55b639c1b3a43e8867caedee67fe
Ave Nurme <nurme.ave@gmail.com>
2022-04-12 18:34:31 +0300

    Finalize the current layout, will automate generating tags with JS

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Project requirements:
+ * - array to hold services requested
+ * - buttons to add service to array
+ * - place to display data from array - updated every time the array changes
+ * - don't charge more than once for same service
+ * - total cost stays updated
+ * - button to "send invoice" (reset after clicking the button)
+ * - STRETCH: remove items after adding - the REMOVE button next to the item added
+ */
