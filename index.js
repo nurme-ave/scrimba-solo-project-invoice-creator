dcf16fea6d469c0921b9deb780bcf624519c7aee
Ave Nurme <nurme.ave@gmail.com>
2022-04-12 20:14:23 +0300

    Render HTML with JS, clean up HTML

--- index.js
+++ index.js
@@ -7,3 +7,31 @@
  * - button to "send invoice" (reset after clicking the button)
  * - STRETCH: remove items after adding - the REMOVE button next to the item added
  */
+
+let services = [
+  {
+    task: 'Wash Car',
+    price: 10,
+  },
+  {
+    task: 'Mow Lawn',
+    price: 20,
+  },
+  {
+    task: 'Pull Weeds',
+    price: 30,
+  },
+];
+
+const tasksHtml = services.map((service) => {
+  return `<p class="task">${service.task}<span class="task-remove">Remove</span></p>`;
+});
+
+document.getElementById('tasks').innerHTML = tasksHtml.join('');
+
+
+const pricesHtml = services.map((service) => {
+  return `<p class="price"><span class="currency-sign">$</span>${service.price}</p>`;
+});
+
+document.getElementById('prices').innerHTML = pricesHtml.join('');
